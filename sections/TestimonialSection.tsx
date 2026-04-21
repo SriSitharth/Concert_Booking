8a7823471d2ced4082b9d43226d91da774003a1b
Sri Sitharth <sitharth@sevmobility.com>
2026-04-21 11:03:33 +0530

    Fixed the seating image

--- sections/TestimonialSection.tsx
+++ sections/TestimonialSection.tsx
@@ -16,7 +16,7 @@ export default function TestimonialSection() {
             >
                 <div className="aspect-square relative bg-white p-4 rounded-xl">
                     <img 
-                        src="/assets/seat.png" 
+                        src="/assets/seating.jpeg" 
                         alt="Seating Arrangement" 
                         className="w-full h-full object-contain"
                     />
